2b2c7327c40d5a0591a7fbd327cb4c400141740a
Emre Koc <hi@emroni.com>
2019-05-12 12:28:46 +0700

    Adjust Hire content

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -26,10 +26,7 @@ export default function Home() {
         </Chapter>
         <Chapter title="For hire" background>
             <Trans tag="p">
-                As I'm currently located in Chiang Mai, Thailand, I work remotely and at a fixed hourly rate.
-            </Trans>
-            <Trans tag="p">
-                For questions or inquiries, feel free to contact me by <Link href="mailto:hi@emroni.com">email</Link> or through <Link href="https://linkedin.com/in/emroni">LinkedIn</Link>.
+                As I'm currently located in Chiang Mai, Thailand, I work remotely and at a fixed hourly rate. For questions or inquiries, feel free to contact me by <Link href="mailto:hi@emroni.com">email</Link> or through <Link href="https://linkedin.com/in/emroni">LinkedIn</Link>.
             </Trans>
         </Chapter>
         <Chapter title="Connect">
