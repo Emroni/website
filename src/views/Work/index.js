edef870de88c27e79a3646a3a143c0b7ded916c2
Emre Koc <hi@emroni.com>
2019-05-12 14:19:18 +0700

    Add The Parc and ROCTOP to Work

--- src/views/Work/index.js
+++ src/views/Work/index.js
@@ -6,6 +6,20 @@ export default function Work() {
 
     const items = [
         {
+            title: 'The Parc',
+            background: 'laptop',
+            color: '#d4ecfc',
+            video: 'the-parc',
+            link: 'https://theparc.eu',
+            summary:
+                <p>
+                    Made with HTML/CSS, JavaScript and PHP<br/>
+                    Designed by <Link href="http://marathonstudio.eu/">Marathon</Link><br/>
+                    Visit <Link href="https://theparc.eu">theparc.eu</Link>
+                </p>,
+            description: 'The Parc - Pharmaceutical Applied Research Center - is a Czech post-graduate program for the next generation of scientists.',
+        },
+        {
             title: 'De Kindertelefoon',
             background: 'tablet',
             color: '#37c9e5',
@@ -20,6 +34,18 @@ export default function Work() {
             description: 'De Kindertelefoon is the national child helpline in The Netherlands. This ad campaign is aimed at kids in need who haven\'t dared to reach out yet. It shows that whatever problems they\'re facing, they\'re not alone. The helpline is there to help in any way they can and can be contacted through several channels.',
         },
         {
+            title: 'ROC TOP Banner Ads',
+            background: 'tablet',
+            color: '#65cdf4',
+            video: 'roctop',
+            summary:
+                <p>
+                    Made with HTML/CSS<br/>
+                    Designed by <Link href="https://wearejust.com/">Just</Link> for <Link href="https://www.roctop.nl/">ROC TOP</Link>
+                </p>,
+            description: 'ROC TOP offers vocational education in over 50 different fields. Running multiple times with different designs and content in a year long campaign, these banner ads are targeted towards new students and their parents.',
+        },
+        {
             title: 'Blockchain Decision Tree',
             background: 'laptop',
             color: '#1B75BA',
@@ -27,7 +53,7 @@ export default function Work() {
             link: 'https://blockchain.humanityx.nl',
             summary:
                 <p>
-                    Made with React and PHP<br/>
+                    Made with React JS, HTML/CSS and PHP<br/>
                     Designed by <Link href="http://marathonstudio.eu/">Marathon</Link> for <Link href="https://www.humanityx.nl/">HumanityX</Link><br/>
                     Visit <Link href="https://blockchain.humanityx.nl">blockchain.humanityx.nl</Link>
                 </p>,
@@ -40,7 +66,7 @@ export default function Work() {
             video: 'applaus-voor-de-zorg',
             summary:
                 <p>
-                    Made with JS and PHP<br/>
+                    Made with JavaScript and PHP<br/>
                     Designed by <Link href="https://www.watch4media.nl/">Watch4Media</Link> for <Link href="https://www.venvn.nl/">V&VN</Link>
                 </p>,
             description: 'V&VN is the professional organisation of Nurse Practitioners in the Netherlands. During the International Nurses Day 2018, this microsite allowed nurses to thank one another for their wonderful work by sending a personalised e-card. Users could upload their photo onto an animated avatar and write a short message. Over 6,000 cards were shared in less than a week.',
