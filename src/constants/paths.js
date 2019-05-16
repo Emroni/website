bba4bcaa2d5bf048919bae785bb5430ecc63124a
Emre Koc <hi@emroni.com>
2019-05-16 23:07:44 +0700

    Adjust paths

--- src/constants/paths.js
+++ src/constants/paths.js
@@ -1,4 +1,4 @@
-export const ASSETS = process.env.REACT_APP_ASSETS_URL;
+export const ASSETS = process.env.REACT_APP_ASSETS_URL || '/assets/';
 
 export function asset(file) {
     let url = `${ASSETS}${file}`;
