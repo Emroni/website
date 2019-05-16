da92fafa1ead9d134a52c4aa7455184057155809
Emre Koc <hi@emroni.com>
2019-05-16 22:38:22 +0700

    Move asset constant to env for share image path

--- src/constants/paths.js
+++ src/constants/paths.js
@@ -1,4 +1,4 @@
-export const ASSETS = process.env.NODE_ENV === 'development' ? '/assets/' : 'https://firebasestorage.googleapis.com/v0/b/emroni-website.appspot.com/o/';
+export const ASSETS = process.env.REACT_APP_ASSETS_URL;
 
 export function asset(file) {
     let url = `${ASSETS}${file}`;
