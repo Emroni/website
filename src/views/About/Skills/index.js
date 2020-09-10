3d5832374d3f039f80bd193e6f5fa6594e17f5b2
Emre Koc <hi@emroni.com>
2020-09-10 10:18:25 +0700

    Update About Skills

--- src/views/About/Skills/index.js
+++ src/views/About/Skills/index.js
@@ -6,8 +6,8 @@ export default function Skills() {
 
     return <Trans className="skills" stall="1">
         <List title="Know" items="JavaScript,HTML/CSS,PHP,After Effects"/>
-        <List title="Love" items="React,ThreeJS,PixiJS,WebSockets"/>
-        <List title="Teach" items="Development,Animation,Mathematics,Physics"/>
+        <List title="Love" items="React,Three.js,PixiJS,D3.js"/>
+        <List title="Teach" items="Development,Administration,Animation,Mathematics"/>
     </Trans>;
 
 }
