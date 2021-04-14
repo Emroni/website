fe06f6b5e9c5a05ce8e7840f50de647b0a1568d4
Emre Koc <hi@emrekoc.io>
2021-04-14 15:36:41 +0700

    Update Image

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,19 +1,28 @@
 import { useEffect, useState } from 'react';
 import classnames from 'classnames';
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles(theme => ({
+    image: {
+        transition: `opacity 0.5s ${theme.eases.inOut}`,
+        '&.loading': {
+            opacity: 0,
+        },
+    },
+}));
 
 export default function Image({ alt, src }) {
 
     const [loading, setLoading] = useState(true);
+    const classes = useStyles();
     
     useEffect(() => {
         const image = new window.Image();
-        image.onload = () => {
-            setLoading(false);
-        };
+        image.onload = () => setLoading(false);
         image.src = src;
     }, [src]);
 
-    const className = classnames('image', {
+    const className = classnames(classes.image, {
         loading,
     });
 
