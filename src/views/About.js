4b21533979078d2372c34fdf42e7a1e012f9a9e6
Emre Koc <hi@emrekoc.io>
2021-04-14 17:00:19 +0700

    Update About

--- src/views/About.js
+++ src/views/About.js
@@ -1,12 +1,28 @@
+import { createUseStyles } from 'react-jss';
 import { Chapter, Link, List, Trans } from '../components';
 
+const useStyles = createUseStyles(theme => ({
+    skills: {
+        margin: '2em -1em',
+        [theme.media.sm]: {
+            display: 'flex',
+            margin: '2em -1.5em',
+        },
+        [theme.media.md]: {
+            margin: '2em -2em',
+        },
+    },
+}));
+
 export default function About() {
+
+    const classes = useStyles();
 
     return <Chapter title="Full Stack Developer">
         <Trans tag="p">
             I build custom frontend and backend solutions, automation tools, motion design and animations. My experience in those fields have also allowed me to mentor dozens of developers with any skill level between intern and senior.
         </Trans>
-        <Trans className="skills" stall={1}>
+        <Trans className={classes.skills} stall={1}>
             <List title="Know" items="JavaScript,PHP,HTML/CSS"/>
             <List title="Love" items="React,Three.js,D3.js"/>
             <List title="Teach" items="Development,Animation,Automation"/>
