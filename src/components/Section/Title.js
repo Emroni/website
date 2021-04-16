c90302af30dc5c62b83d262cb129d4cddb147871
Emre Koc <hi@emrekoc.io>
2021-04-16 18:00:43 +0700

    Replace Section Title chevrons with svgs

--- src/components/Section/Title.js
+++ src/components/Section/Title.js
@@ -1,61 +1,41 @@
 import { createUseStyles } from 'react-jss';
 import { Trans } from '../index';
 
-// TODO: Replace chevrons with SVG icons
 const useStyles = createUseStyles({
     title: {
         display: 'inline-block',
         marginBottom: '1em',
         position: 'relative',
+    },
+    content: {
+        display: 'inline-block',
+        overflow: 'hidden',
         '& > span': {
             display: 'inline-block',
-            overflow: 'hidden',
-            '& > span': {
-                display: 'inline-block',
-                transition: 'opacity 0.5s var(--ease) 0.25s, transform 0.5s var(--ease) 0.25s',
-            },
+            transition: 'opacity 0.5s var(--ease) 0.25s, transform 0.5s var(--ease) 0.25s',
         },
-        '&:before, &:after': {
-            backgroundPosition: 'center',
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: '100%',
-            content: '""',
-            position: 'absolute',
-            top: '50%',
+        '$title:not(.active) & > span': {
+            opacity: 0,
             transform: 'translateY(-50%)',
-            transition: 'opacity 0.5s var(--ease), transform 0.5s var(--ease)',
         },
-        '&:before': {
-            backgroundImage: 'url(/images/code-before-black.png)',
-            height: '0.8em',
-            left: '-0.75em',
-            width: '0.4375em',
-            '.background &': {
-                backgroundImage: 'url(/images/code-before-white.png)',
-            },
+    },
+    svg: {
+        position: 'absolute',
+        top: 0,
+        transition: 'opacity 0.5s var(--ease), transform 0.5s var(--ease)',
+        '&:first-child': {
+            left: '-16px',
         },
-        '&:after': {
-            backgroundImage: 'url(/images/code-after-black.png)',
-            height: '1.125em',
-            right: '-1.125em',
-            width: '0.875em',
-            '.background &': {
-                backgroundImage: 'url(/images/code-after-white.png)',
-            },
+        '&:last-child': {
+            right: '-25px',
         },
-        '&:not(.active)': {
-            '& > span > span': {
-                opacity: 0,
-                transform: 'translateY(-50%)',
-            },
-            '&:before': {
-                opacity: 0,
-                transform: 'translate(200%, -50%)',
-            },
-            '&:after': {
-                opacity: 0,
-                transform: 'translate(-200%, -50%)',
-            },
+        '$title:not(.active) &:first-child': {
+            opacity: 0,
+            transform: 'translateX(20px)',
+        },
+        '$title:not(.active) &:last-child': {
+            opacity: 0,
+            transform: 'translateX(-20px)',
         },
     },
 });
@@ -65,9 +45,16 @@ export default function Section({children}) {
     const classes = useStyles();
 
     return <Trans className={classes.title} fade={false} stall={0.5} tag="h2">
-        <span>
+        <svg className={classes.svg} width="11px" height="24px" viewBox="0 0 7 16">
+            <polygon fill="currentColor" points="7 2.38923077 5.45 1 0 8 5.45 15 7 13.6107692 2.61 8"/>
+        </svg>
+        <span className={classes.content}>
             <span>{children}</span>
         </span>
+        <svg className={classes.svg} width="21px" height="24px" viewBox="0 0 14 16">
+            <polygon fill="currentColor" points="14 2.38923077 12.45 1 7 8 12.45 15 14 13.6107692 9.61 8" transform="rotate(180) translate(-20, -16)"/>
+            <polygon fill="currentColor" points="0 15.5806452 1.63098592 16 6 0.419354839 4.36901408 0"/>
+        </svg>
     </Trans>;
 
 }
