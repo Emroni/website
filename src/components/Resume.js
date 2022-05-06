ea5819db34c1a90158870ba3d28e9ec25315b1c7
Emre Koc <hi@emrekoc.io>
2022-05-06 11:31:27 +0700

    Remove previous version

--- src/components/Resume.js
+++
@@ -1,9 +0,0 @@
-import { Link, Trans } from './index';
-
-export default function Resume() {
-
-    return <Trans tag="p">
-        <Link href="/assets/Emre-Koc-Resume.pdf">Download Resume (PDF)</Link>
-    </Trans>;
-
-}
