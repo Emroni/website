b264dc016aea588518d209ec8e7e6602b4c70674
Emre Koc <hi@emrekoc.io>
2022-05-19 14:36:13 +0700

    Convert component imports to exports

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,13 +1,5 @@
-import Link from './Link';
-import List from './List';
-import Resume from './Resume';
-import Section from './Section';
-import Trans from './Trans';
-
-export {
-    Link,
-    List,
-    Resume,
-    Section,
-    Trans,
-}
+export { default as Link } from './Link';
+export { default as List } from './List';
+export { default as Resume } from './Resume';
+export { default as Section } from './Section';
+export { default as Trans } from './Trans';
