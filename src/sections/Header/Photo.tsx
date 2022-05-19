73e1797bdbe839877df6dca22cb1d7a1f850566f
Emre Koc <hi@emrekoc.io>
2022-05-19 10:26:21 +0700

    Fix Header

--- src/sections/Header/Photo.tsx
+++ src/sections/Header/Photo.tsx
@@ -52,7 +52,7 @@ export default function Photo() {
 
     const [loading, setLoading] = useState(true);
     const classes = useStyles();
-    const src = '/images/emre-koc.png';
+    const src = 'assets/images/emre-koc.png';
 
     useEffect(() => {
         const image = new window.Image();
