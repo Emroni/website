243d91a510a6ba0b5d86ec0ee3feaba677f79fe2
Emre Koc <hi@emrekoc.io>
2024-07-19 17:40:44 +0200

    Rename partials to sections

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { Header } from 'components';
 import Head from 'next/head';
-import { Clients, Connect, Education, Experience, Intro, Knowledge } from 'partials';
+import { Clients, Connect, Education, Experience, Intro, Knowledge } from 'sections';
 
 export default function Home() {
 
