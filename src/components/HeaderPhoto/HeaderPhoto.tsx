69cc46992c31361d23b2c2d9c1deaf19a3e72ec8
Emre Koc <hi@emrekoc.io>
2024-07-19 17:56:02 +0200

    Restore HeaderPhoto svg class

--- src/components/HeaderPhoto/HeaderPhoto.tsx
+++ src/components/HeaderPhoto/HeaderPhoto.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import { createUseStyles } from 'react-jss';
 
 const useStyles = createUseStyles({
+    svg: {},
     border: {
         strokeDasharray: '0, 157%',
         '$svg.active &': {
@@ -38,7 +39,7 @@ export default function HeaderPhoto() {
     });
 
     return <div className="h-14 mr-6 overflow-hidden relative rounded-full w-14 xs:h-20 xs:mr-8 xs:w-20 sm:h-24 sm:mr-12 sm:w-24 md:h-28 md:mr-14 md:w-28 lg:h-32 lg:mr-14 lg:w-32 xl:h-36 xl:mr-16 xl:w-36">
-        <Transition className="absolute fill-transparent h-full left-0 top-0 w-full z-10" fade={false} tag="svg">
+        <Transition className={`absolute fill-transparent h-full left-0 top-0 w-full z-10 ${classes.svg}`} fade={false} tag="svg">
             <circle className={`opacity-0 stroke-1.5 stroke-blue-500 xs:stroke-2 sm:stroke-3 xl:stroke-4 ${classes.border}`} cx="50%" cy="50%" r="50%" />
         </Transition>
         <picture>
