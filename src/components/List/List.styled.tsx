945afe5afacf4bd094441f75f8abe9116330a776
Emre Koc <hi@emrekoc.io>
2025-06-12 11:11:59 +0200

    Style List

---
+++ src/components/List/List.styled.tsx
@@ -0,0 +1,77 @@
+import _ from 'lodash';
+import styled, { css } from 'styled-components';
+
+export const Wrap = styled.div`
+    position: relative;
+
+    &:before {
+        background-color: currentColor;
+        content: '';
+        height: 0;
+        left: -16px;
+        position: absolute;
+        top: 0;
+        transition: height 0.5s var(--ease);
+        width: 1px;
+    }
+
+    &.active:before {
+        height: 100%;
+    }
+`;
+
+export const Heading = styled.h4`
+    font-size: 18px;
+    font-weight: 600;
+    margin: 0;
+    transition: opacity 0.5s var(--ease), transform 0.5s var(--ease);
+
+    .wrap:not(.active) & {
+        opacity: 0;
+        transform: translateX(-4px);
+    }
+`;
+
+export const Container = styled.ul`
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+`;
+
+export const Item = styled.li`
+    font-size: 18px;
+    margin-left: -4px;
+    padding: 4px;
+    position: relative;
+    transition: opacity 0.5s var(--ease), transform 0.5s var(--ease);
+
+    &:before {
+        background-color: var(--blue-500);
+        content: '';
+        height: 100%;
+        left: 0;
+        opacity: 0;
+        position: absolute;
+        top: 0;
+        transition: opacity 0.25s var(--ease);
+        width: 100%;
+        z-index: -1;
+    }
+
+    &:hover:before {
+        opacity: 0.05;
+    }
+
+    ${_.range(1, 15).map(
+        i => css`
+            &:nth-child(${i}) {
+                transition-delay: ${0.05 * i}s;
+            }
+        `
+    )}
+
+    .wrap:not(.active) & {
+        opacity: 0;
+        transform: translateX(-4px);
+    }
+`;
--- src/sections/Experience.tsx
+++ src/sections/Experience.tsx
@@ -11,92 +11,60 @@ const classes = {
 };
 
 export default function Experience() {
-    // const classes = useStyles();
-
     return (
         <Section heading="Work Experience">
             <div>
                 <Transition className={`text-blue-500 ${classes.title}`} fade={false} tag="h3">
                     2018 - Present | Freelancer
                 </Transition>
-                <List className="mt-4">
-                    <li>
-                        Developed frontend of public websites and internal portals in React, making use of frameworks
-                        like Material-UI and Next.js
-                    </li>
-                    <li>
-                        Developed backend systems in PHP with Symfony, Laravel and Zend frameworks, using Twig and Blade
-                        templating engines
-                    </li>
-                    <li>Built custom content management systems for managing data in SQL databases</li>
-                    <li>Expanded, restructured and implemented speed improvements in existing applications</li>
-                    <li>
-                        Built automation tools for generating APIs for Azure Functions based on routines export from SQL
-                        database
-                    </li>
-                    <li>
-                        Implemented extensive SEO improvements using microdata and JSON-LD, based on Schema.org&apos;s
-                        vocabulary
-                    </li>
-                    <li>Designed mockups and wireframes in Sketch and Illustrator</li>
-                    <li>Mentored juniors in React and development in general on weekly basis</li>
-                    <li>Hosted monthly developer meetups, giving talks about a career in web development</li>
-                </List>
+                <List
+                    items={[
+                        'Developed frontend of public websites and internal portals in React, making use of frameworks like Material-UI and Next.js',
+                        'Developed backend systems in PHP with Symfony, Laravel and Zend frameworks, using Twig and Blade templating engines',
+                        'Built custom content management systems for managing data in SQL databases',
+                        'Expanded, restructured and implemented speed improvements in existing applications',
+                        'Built automation tools for generating APIs for Azure Functions based on routines export from SQL database',
+                        "Implemented extensive SEO improvements using microdata and JSON-LD, based on Schema.org's vocabulary",
+                        'Designed mockups and wireframes in Sketch and Illustrator',
+                        'Mentored juniors in React and development in general on weekly basis',
+                        'Hosted monthly developer meetups, giving talks about a career in web development',
+                    ]}
+                />
             </div>
             <div className="mt-8">
                 <Transition className={`text-blue-500 ${classes.title}`} fade={false} tag="h3">
                     2009 - 2018 | Full-time @ Just
                 </Transition>
-                <List className="mt-4" title="2014 | Digital Lead">
-                    <li>
-                        Led department of frontend and backend development, responsible for quality control of all
-                        deliverables
-                    </li>
-                    <li>
-                        Acted as bridge between project management and development team by handling all technical
-                        questions by project mangers, clients and other agencies
-                    </li>
-                    <li>
-                        Mentored interns, junior and senior developers in all in-house used programming languages,
-                        frameworks and tools
-                    </li>
-                    <li>Consulted clients on processes and architecture based on project requirements</li>
-                    <li>Developed frontend of websites and applications with React</li>
-                    <li>Built real-time interactive multi-user applications using WebSockets</li>
-                    <li>Built 2D/3D animations in JavaScript for websites</li>
-                    <li>
-                        Built automation tools with JavaScript (Node) and PHP for significantly faster development and
-                        delivery of advertising banners
-                    </li>
-                    <li>
-                        Developed an internal application with automated repeating tasks, significantly reducing
-                        workload for HR, while providing more transparency to employees
-                    </li>
-                    <li>
-                        Developed a standardized cookie bar with implementation documentation for use by government
-                        agencies
-                    </li>
-                    <li>
-                        Created motion graphics in After Effects for advertising videos, making use of ExtendScript to
-                        generate variations
-                    </li>
-                </List>
-                <List className="mt-4" title="2011 | Full Stack Developer">
-                    <li>Led small teams of frontend and backend developers</li>
-                    <li>Mentored junior developers in JavaScript and Flash</li>
-                    <li>
-                        Developed frontend of websites with JavaScript, HTML and CSS from designs in Sketch and
-                        Illustrator
-                    </li>
-                    <li>
-                        Developed backend systems in PHP for websites and internal applications, using the Symfony,
-                        Laravel and CodeIgniter frameworks, with Twig and Blade templating engines
-                    </li>
-                    <li>Built motion graphics for videos and animations for websites in Flash</li>
-                </List>
-                <List className="mt-4" title="2009 | Flash Developer">
-                    <li>Built and implemented components and full websites in Flash from designs in Photoshop</li>
-                </List>
+                <List
+                    title="2014 | Digital Lead"
+                    items={[
+                        'Led department of frontend and backend development, responsible for quality control of all deliverables ',
+                        'Acted as bridge between project management and development team by handling all technical questions by project mangers, clients and other agencies',
+                        'Mentored interns, junior and senior developers in all in-house used programming languages, frameworks and tools',
+                        'Consulted clients on processes and architecture based on project requirements',
+                        'Developed frontend of websites and applications with React',
+                        'Built real-time interactive multi-user applications using WebSockets',
+                        'Built 2D/3D animations in JavaScript for websites',
+                        'Built automation tools with JavaScript (Node) and PHP for significantly faster development and delivery of advertising banners',
+                        ' Developed an internal application with automated repeating tasks, significantly reducing workload for HR, while providing more transparency to employees',
+                        'Developed a standardized cookie bar with implementation documentation for use by government agencies',
+                        'Created motion graphics in After Effects for advertising videos, making use of ExtendScript to generate variations',
+                    ]}
+                />
+                <List
+                    title="2011 | Full Stack Developer"
+                    items={[
+                        'Led small teams of frontend and backend developers',
+                        'Mentored junior developers in JavaScript and Flash',
+                        'Developed frontend of websites with JavaScript, HTML and CSS from designs in Sketch and Illustrator',
+                        'Developed backend systems in PHP for websites and internal applications, using the Symfony, Laravel and CodeIgniter frameworks, with Twig and Blade templating engines',
+                        'Built motion graphics for videos and animations for websites in Flash',
+                    ]}
+                />
+                <List
+                    title="2009 | Flash Developer"
+                    items={['Built and implemented components and full websites in Flash from designs in Photoshop']}
+                />
             </div>
             <Transition className="mt-4 sm:mt-6 md:mt-8" tag="p">
                 <Link href="/assets/Emre-Koc-Resume.pdf">Download Resume (PDF)</Link>
