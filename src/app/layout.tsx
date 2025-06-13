2d5ec4f7bc280ad7ce9853e304add468c869c318
Emre Koc <hi@emrekoc.io>
2025-06-13 08:56:42 +0200

    Update resume and share photos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,16 +5,17 @@ import Registry from './registry';
 import './styles.css';
 
 export const metadata: Metadata = {
-    title: 'Emre Koc - Full Stack Developer',
     description: 'Full Stack Developer',
-    // TODO: Add og metadata
-    // property="og:url" content="https://emrekoc.io" />
-    // property="og:description" content="Full Stack Developer" />
-    // property="og:site_name" content="Emre Koc" />
-    // property="og:title" content="Emre Koc" />
-    // property="og:type" content="website" />
-    // property="og:image" content="/assets/share.png" />
-    // name="theme-color" content="#ffffff" />
+    openGraph: {
+        description: 'Full Stack Developer',
+        images: '/assets/share.png',
+        siteName: 'Emre Koc',
+        title: 'Emre Koc',
+        type: 'website',
+        url: 'https://emrekoc.io',
+    },
+    themeColor: '#ffffff',
+    title: 'Emre Koc - Full Stack Developer',
 };
 
 export const viewport: Viewport = {
