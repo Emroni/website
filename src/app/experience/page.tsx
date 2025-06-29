0c690cff74e4139cdf3b4b4cfdc57b53c3d0cb1b
Emre Koc <hi@emrekoc.io>
2025-06-29 14:22:00 +0200

    Add experience and work pages

---
+++ src/app/experience/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function Experience() {
+    return (
+        <>
+            <main>Experience</main>
+        </>
+    );
+}
---
+++ src/app/work/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function Work() {
+    return (
+        <>
+            <main>Work</main>
+        </>
+    );
+}
