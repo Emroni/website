17a33eed21ba671ebf3bb879de7de5e7389dce2c
Emre Koc <hi@emrekoc.io>
2025-08-30 16:54:03 +0200

    Update content

--- src/sections/Knowledge/index.tsx
+++ src/sections/Knowledge/index.tsx
@@ -8,11 +8,11 @@ export default function Knowledge() {
             <Transition component={Container} fade={false} stall={0.1}>
                 <KnowledgeBlock
                     title="Extensive"
-                    items="TypeScript,JavaScript,React,Node.js,Next.js,Jest,D3.js,Pixi.js,Three.js,Material UI,SQL,HTML,CSS,REST APIs,Data visualization,Generators,Automation,Motion Design"
+                    items="TypeScript,JavaScript,React,Node.js,Next.js,Jest,D3.js,Pixi.js,Three.js,Material UI,HTML,CSS,REST APIs,Data visualization,Generators"
                 />
                 <KnowledgeBlock
                     title="Proficient"
-                    items="Redux,Sentry,Figma,Tailwind,Storybook,Swagger,Websockets,GraphQL,OpenAI,Azure,GitHub Actions,Heroku,Docker"
+                    items="Redux,Sequelize,Figma,Playwright,SQL,Sentry,Tailwind,Storybook,Swagger,Websockets,GraphQL,OpenAI,Azure,GitHub Actions,Heroku,Docker,Automation,Motion Design"
                 />
             </Transition>
             <ResumeLink />
