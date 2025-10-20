a9f2ac65dfb892f8234b5b9f74d50ec0d23cc5ee
Emre Koc <hi@emrekoc.io>
2025-10-20 16:00:59 +0200

    Update Knowledge

--- src/sections/Knowledge/index.tsx
+++ src/sections/Knowledge/index.tsx
@@ -6,14 +6,16 @@ export default function Knowledge() {
     return (
         <Section title="Technical Knowledge">
             <Transition component={Container} fade={false} stall={0.1}>
+                <KnowledgeBlock title="Foundation" items="TypeScript,JavaScript,PHP,HTML,CSS" />
                 <KnowledgeBlock
-                    title="Extensive"
-                    items="TypeScript,JavaScript,React,Node.js,Next.js,Jest,D3.js,Pixi.js,Three.js,Material UI,HTML,CSS,REST APIs,Data visualization,Generators"
+                    title="Frontend"
+                    items="React,Next.js,Material UI,Tailwind,Redux,D3.js,Pixi.js,Three.js"
                 />
-                <KnowledgeBlock
-                    title="Proficient"
-                    items="Redux,Sequelize,Figma,Playwright,SQL,Sentry,Tailwind,Storybook,Swagger,Websockets,GraphQL,OpenAI,Azure,GitHub Actions,Heroku,Docker,Automation,Motion Design"
-                />
+                <KnowledgeBlock title="Backend" items="Node.js,Symfony,Laravel,REST,GraphQL,WebSockets" />
+                <KnowledgeBlock title="Database" items="PostgreSQL,MySQL,Firebase,Sequelize,Prisma" />
+                <KnowledgeBlock title="Testing" items="Jest,Playwright,Storybook,Chromatic" />
+                <KnowledgeBlock title="Deployment" items="Docker,Kubernetes,AWS,Azure,Heroku,GitHub Actions" />
+                <KnowledgeBlock title="Tools" items="Sentry,Swagger,OpenAI,Figma" />
             </Transition>
             <SectionLinks>
                 <Link href="https://experimental.emrekoc.io/">Experimental Artwork</Link>
